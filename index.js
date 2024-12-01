01f4f6d9a60fae44456d5635a377eabb66e27be1
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2024-12-01 13:11:19 +0000

    changes

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require("express");
 const Movie = require("./movie.schema");
 
 const app = express();
-const MONGO_URI = process.env.MONGO_URI;
-console.log({ MONGO_URI });
+
+// connect to database
+const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost";
 mongoose
   .connect(MONGO_URI)
   .then((conn) => {
@@ -15,10 +16,14 @@ mongoose
     console.log({ err });
   });
 
+
+
+
+
 app.get("/", async (_req, res) => {
   const data = await Movie.find();
   return res.json({ data });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => console.log(`running on ${PORT}`));
